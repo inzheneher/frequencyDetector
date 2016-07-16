38f20db74caeefcb643d2b2b105c1d672c99d26f
inzheneher <inzheneher@ya.ru>
2016-07-16 18:58:05 +0600

    connecting people worked program

---
+++ ConnectingPeople/ConnectingPeople/ConnectingPeople.ino
@@ -0,0 +1,70 @@
+
+const int ledPin =  12;
+
+boolean trigger = false;
+
+unsigned long count = 0;
+unsigned long currentCount = 0;
+
+int ledState = LOW;        
+
+int lowRange = 5000;
+int highRange = 5050;
+
+unsigned long previousMillis = 0;
+unsigned long countPreviousMillis = 0;
+
+const long interval = 50;
+const long countInterval = 1000;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(3,INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(3) == LOW) {
+    trigger = true;
+  }
+
+  if (trigger) {
+      analogWrite(11, random(0, 25));
+    
+  unsigned long currentMillis = micros();
+  unsigned long countCurrentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    
+    previousMillis = currentMillis;
+
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(ledPin, ledState);
+    
+    if (analogRead(A0) >= 450) {
+      count++;
+      tone(13, 35, 500);
+    } 
+  }
+
+    if (countCurrentMillis - countPreviousMillis >= countInterval) {
+      countPreviousMillis = countCurrentMillis;
+
+      
+      if (count >= lowRange) {
+        digitalWrite(2, HIGH);
+        delay(3000);
+        digitalWrite(2, LOW);
+        count = 0;
+      }
+      if (count == currentCount) {
+        count = 0;
+      }
+      currentCount = count;
+    }  
+  }
+}
+
--- FadingWithoutDelay/FadingWithoutDelay/FadingWithoutDelay.ino
+++ FadingWithoutDelay/FadingWithoutDelay/FadingWithoutDelay.ino
@@ -8,15 +8,16 @@ unsigned int t_delay = 1;
 int loopStep = 1;
 int fadeValue = 0;
 int counter = 0;
+int ledState = LOW;
 
 unsigned long previousMillis = 0;        // will store last time LED was updated
-const long interval = 3;           // 1/0,000349 - детектируемая частота
+const long interval = 10000;           // 1/0,000349 - детектируемая частота
 
 unsigned long detectedPreviousMillis = 0;
 const long detectedInterval = 5;
  
 unsigned long basePreviousMillis = 0;
-int detectedPulseAmountLowRange = 14;
+int detectedPulseAmountLowRange = 11;
 int detectedPulseAmountHighRange = 20; //high range of detected pulses 32767
 const long baseInterval = 100;
 
@@ -27,7 +28,8 @@ void setup() {
 }
 
 void loop() {
-  
+meandr();
+ /* 
   unsigned long baseCurrentMillis = millis();
   
   fadeUp();
@@ -48,7 +50,7 @@ void loop() {
     } else {
       counter = 0;
     }
-  }
+  }*/
 }
 
 void fadeUp(){
@@ -82,8 +84,8 @@ void fadeDown(){
 }
 
 boolean pickDetector(){
-  if (analogRead(A0) > 900) {
-    if (analogRead(A0) > 900) {
+  if (analogRead(A0) > 800) {
+    if (analogRead(A0) > 800) {
       return true; 
     }
   }
@@ -106,5 +108,24 @@ void intervalDetector (int count) {
       digitalWrite(2, HIGH);
     }
   }
+}
+
+void meandr() {
+  unsigned long currentMillis = micros();
+    
+    if (currentMillis - previousMillis >= interval) {
+        
+        previousMillis = currentMillis;
+    
+      if (ledState == LOW) {
+        ledState = HIGH;
+      } else {
+        ledState = LOW;
+      }
+  
+      // set the LED with the ledState of the variable:
+      digitalWrite(ledPin, ledState);
+      Serial.println(digitalRead(3));
+    }
 }
 
